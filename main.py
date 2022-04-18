c3971a42097cbd8ca3b329e4b2512b0c87d96e7e
ThomasCardin <tomcardin@outlook.com>
2022-04-18 15:21:39 -0400

    Removing x < 127 char

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Return max value between 2 values
 def max(a, b):
-    # this_will_cause_a_error = "asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
     if a > b:
         # for i in 10:
         #     print(i)

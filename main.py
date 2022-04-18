c6ea9703cc28668cb12161c6e165a0ae5ab169e6
ThomasCardin <tomcardin@outlook.com>
2022-04-18 15:08:11 -0400

    causing linting error

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import math
 import pytest
 
 # Return max value between 2 values
-def max(a      , b):
+def max(a, b):
+    this_will_cause_a_error = "asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
     if a > b:
         return a
     else:

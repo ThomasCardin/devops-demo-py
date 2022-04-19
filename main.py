3d4887f26aa46811e71cc5f65dfdc7f85282498c
ThomasCardin <49320132+ThomasCardin@users.noreply.github.com>
2022-04-19 09:35:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 # Return max value between 2 values
-def max(a, b):
-    if a > b:
-        # for i in 10:
-        #     print(i)
+def max(a                          , b):
+    test = "asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+    if a < b:
+        for i in 10:
+            print(i)
         return a
     else:
         return b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a38f66b922fee184e5f1f8a5dcf2a961a2af356a
ThomasCardin <tomcardin@outlook.com>
2022-09-25 20:17:53 -0400

    added new requests

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +6,17 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello_world():
-    return "<p> Hello world <p>"
+    return "Main: " + "Hello from main!"
+
+
+@app.route("/helloDocker")
+def hello_world_docker():
+    docker_message = ""
+    try:
+        docker_message = os.environ("DOCKER_ENV")
+    except Exception:
+        docker_message = "docker env not found"
+    return "Docker: " + docker_message
 
 
 # Return max value between 2 values
@@ -16,6 +27,3 @@ def max(a, b):
         return a
     else:
         return b
-
-
-print("test")
